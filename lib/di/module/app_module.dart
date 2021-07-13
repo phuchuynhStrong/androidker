641f9f64762cfac1a0a92f4a98c75dafba95885b
Phuc Huynh <phuchuynh.strong@gmail.com>
2021-07-13 11:35:33 +0700

    - Remove leading hash - Inject AppComponent which contains SharedPrefs

---
+++ lib/di/module/app_module.dart
@@ -0,0 +1,7 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AppModule {
+  final SharedPreferences? sharedPreferences;
+
+  AppModule({this.sharedPreferences});
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:androiker/_utils/logger.dart';
+import 'package:androiker/di/component/app_component.dart';
+import 'package:androiker/di/module/app_module.dart';
 import 'package:androiker/routing/app_route_parser.dart';
 import 'package:androiker/routing/app_router.dart';
+import 'package:androiker/routing/bloc/routing_bloc.dart';
 import 'package:androiker/services/firebase/firebase_service.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'routing/cubit/routing_cubit.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() async {
   initLogger(() async {
@@ -26,12 +30,28 @@ void main() async {
 
     FirebaseFactory.create();
 
-    runApp(const _AndroidkerApp());
+    // Reemove leading hash (#) from web URl
+    setPathUrlStrategy();
+    // Get SharedPreferences instance and inject into App
+    final sharedPrefs = await SharedPreferences.getInstance();
+    // Initialize AppComponent
+    final appComponent = AppComponent(
+        appModule: AppModule(
+      sharedPreferences: sharedPrefs,
+    ));
+
+    runApp(_AndroidkerApp(
+      appComponent: appComponent,
+    ));
   });
 }
 
 class _AndroidkerApp extends StatefulWidget {
-  const _AndroidkerApp({Key? key}) : super(key: key);
+  final AppComponent? appComponent;
+  const _AndroidkerApp({
+    Key? key,
+    this.appComponent,
+  }) : super(key: key);
 
   @override
   _AndroidkerAppState createState() => _AndroidkerAppState();
@@ -40,23 +60,28 @@ class _AndroidkerApp extends StatefulWidget {
 class _AndroidkerAppState extends State<_AndroidkerApp> {
   AppRouteParser appRouteParser = AppRouteParser();
   late AppRouterDelegate router;
-  late RoutingCubit routingCubit;
+  late RoutingBloc routingBloc;
 
   @override
   void initState() {
-    routingCubit = RoutingCubit();
-    router = AppRouterDelegate(routingCubit);
+    routingBloc = RoutingBloc(
+      sharedPreferences: widget.appComponent?.appModule?.sharedPreferences,
+    );
+    router = AppRouterDelegate(routingBloc);
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => routingCubit,
-      child: MaterialApp.router(
-        debugShowCheckedModeBanner: false,
-        routeInformationParser: appRouteParser,
-        routerDelegate: router,
+    return Provider<AppComponent?>(
+      create: (_) => widget.appComponent,
+      child: BlocProvider(
+        create: (context) => routingBloc,
+        child: MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          routeInformationParser: appRouteParser,
+          routerDelegate: router,
+        ),
       ),
     );
   }
