20aac2e747aa109f23919788fc2778188ad0a138
Phuc Huynh <phuchuynh.strong@gmail.com>
2021-07-25 16:13:57 +0700

    Add Article module - Take responsibility for article model, repo, request

---
+++ packages/article/lib/model/article.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'article.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Article _$_$_ArticleFromJson(Map<String, dynamic> json) {
+  return _$_Article(
+    id: json['id'] as String?,
+    title: json['title'] as String?,
+    cover: json['cover'] as String?,
+    thumbnail: json['thumbnail'] as String?,
+    content: json['content'] as String?,
+    author: json['author'] as String?,
+  );
+}
+
+Map<String, dynamic> _$_$_ArticleToJson(_$_Article instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'cover': instance.cover,
+      'thumbnail': instance.thumbnail,
+      'content': instance.content,
+      'author': instance.author,
+    };
